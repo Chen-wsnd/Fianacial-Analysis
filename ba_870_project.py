960d1e3f7e83defde5c27e75c264045e381e167e
Mishil Trivedi <trivedimishil31@gmail.com>
2025-04-25 10:33:14 -0400

    Update ba_870_project.py

--- ba_870_project.py
+++ ba_870_project.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-st.markdown("<div style='position: absolute; top: 10px; left: 10px; font-size: 12px; color: gray;'>Created by Mishil Trivedi and Junhan Chen</div>", unsafe_allow_html=True)
 st.set_page_config(page_title="Insider Trade Suspicion Scorer", layout="centered")
+
+st.markdown("<div style='position: absolute; top: 10px; left: 10px; font-size: 12px; color: gray;'>Created by Mishil Trivedi and Junhan Chen</div>", unsafe_allow_html=True)
 
 st.title("📊 Insider Trade Suspicion Scorer")
 st.markdown("Enter trade details below to get a suspicion score and classification.")
